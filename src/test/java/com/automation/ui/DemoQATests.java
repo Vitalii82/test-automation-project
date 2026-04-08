133d65fa02346b92b096013e6d64f21db1ff9afa
Vitalii82 <svinovej90@gmail.com>
2026-04-08 20:56:55 +0300

    Update DemoQATests setup

--- src/test/java/com/automation/ui/DemoQATests.java
+++ src/test/java/com/automation/ui/DemoQATests.java
@@ -1,27 +1,47 @@
 package com.automation.ui.tests;
 
+import com.automation.ui.pages.TextBoxPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.*;
-import com.automation.ui.pages.TextBoxPage;
 
 public class DemoQATests {
     private WebDriver driver;
     private TextBoxPage textBoxPage;
 
-    @BeforeClass
-    public void setup() {
-        driver = new ChromeDriver();
-        textBoxPage = new TextBoxPage(driver);
+    @BeforeClass(alwaysRun = true)
+    public void setUp() {
+        try {
+            // Setup Chrome driver automatically
+            WebDriverManager.chromedriver().setup();
+
+            // Safe Chrome options (avoid crash in headless, OneDrive, etc.)
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+
+            textBoxPage = new TextBoxPage(driver);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("Failed to initialize WebDriver: " + e.getMessage());
+        }
     }
 
     @Test
-    public void testTextBoxForm() {
+    public void fillTextBoxForm() {
         textBoxPage.open();
-        textBoxPage.fillForm("Vitalii", "test@mail.com", "Lutsk", "Ukraine");
+        textBoxPage.fillForm("Vitalii", "demo@mail.com", "Lutsk", "Ukraine");
     }
 
-    @AfterClass
+    @AfterClass(alwaysRun = true)
     public void tearDown() {
         if (driver != null) {
             driver.quit();
